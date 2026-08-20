fe96b53caef7722567fd9da2bc509204147d7d92
qsa <15857667663@139.com>
2026-08-20 18:57:25 +0800

    feat: import src/vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+  readonly VITE_APP_URL: string;
+  readonly VITE_OCR_LIMIT: string;
+  readonly VITE_TURNSTILE_SITE_KEY?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
